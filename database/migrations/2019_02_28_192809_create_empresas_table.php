e6c4b8fa3ccdc54a170e2dc00d95d5a49bd55968
Francisco Javier Rios Bautista <fjrb44@gmail.com>
2019-02-28 22:07:42 +0100

    Creacion de los controllers
    Se han creado los controladores y sus respectivas relaciones.

---
+++ database/migrations/2019_02_28_192809_create_empresas_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresasTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('logo');
+            
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
---
+++ database/migrations/2019_02_28_192918_create_empleados_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email')->unique();
+
+            $table->integer('empresa_id');
+            $table->foreign('empresa_id')->references('id')->on('empresas')
+                ->onUpdate("cascade")->onDelete("cascade");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
